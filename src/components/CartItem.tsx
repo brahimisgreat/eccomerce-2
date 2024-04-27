7f075afdf46160e2a3687ff46048434195a1c789
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-04-26 23:12:46 -0400

    yes

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { list } from "../Data/Items"
 import { Stack } from "react-bootstrap"
 import { useShoppingCart } from "../Context/ShoppingCartcontext"
+import { Button } from "react-bootstrap"
 
 type CartItemProps = { id: number, quantity: number }
 
@@ -14,8 +15,20 @@ export const CartItem = ({id, quantity} : CartItemProps) => {
     
     
   return (
-    <Stack gap={3}>
-      <img src={item.image}/>
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
+      <img src={item.image} alt={item.title} style={{width: "125px", height:'75px', objectFit:'contain'}} />
+      <div className="me-auto">
+        <div>
+          {item.title}  {quantity > 1 && <span className="text-muted" style={{fontSize:'.65rem'}}>x{quantity}</span>}
+        </div>
+        <div style={{fontSize:'.65rem'}}>
+          {item.price} <span>ea</span>
+        </div>
+        <div>
+          <div>${item.price * quantity}</div>
+          <Button variant="danger" size="sm" onClick={() => removeFromCart(id)}>Remove</Button>
+        </div>
+      </div>
     </Stack>
   )
 }
